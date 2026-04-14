a8a0d534883f2765809d1f1b9b96dd0862d86203
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-14 13:07:09 -0500

    Feat: output code cellphone app

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -4,8 +4,8 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class CellPhoneApplication {
-    public static void main(String[] args) {
-Scanner input = new Scanner(System.in);
+    public static CellPhone main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
 
 //CREATE BLANK PHONE:
@@ -27,8 +27,21 @@ Scanner input = new Scanner(System.in);
         System.out.print("Who is this phone registered to?");
         String phoneOwner = input.next();
 
+//Setters:
+        cellphone.setSerialNumber(serialNumber);
+        cellphone.setPhoneModel(phoneModel);
+        cellphone.setPhoneCarrier(phoneCarrier);
+        cellphone.setPhoneNumber(phoneNumber);
+        cellphone.setPhoneOwner(phoneOwner);
 
-
+//        OUTPUT:
+        System.out.println("\n ~~Phone Records~~");
+        System.out.println("Serial Number: " + cellphone.getSerialNumber());
+        System.out.println("Model: " + cellphone.getPhoneModel());
+        System.out.println("Phone Carrier: " + cellphone.getPhoneCarrier());
+        System.out.println("Phone Number:" + cellphone.getPhoneNumber());
+        System.out.println("Phone Owner: " + cellphone.getPhoneOwner());
+        return cellphone;
 
 
     }
